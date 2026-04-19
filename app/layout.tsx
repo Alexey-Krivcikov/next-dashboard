daac3bdd458311313b56c38f040afbe71b587ece
Aleksey Krivchikov <krivchikovalexei@yandex.ru>
2026-04-19 11:39:13 +0300

    task: added eslint, prettier, fonts, images

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,12 @@
-import '@/app/ui/global.css';
 import React from "react";
+import "@/app/ui/global.css";
+import { inter } from "@/app/ui/fonts";
+import clsx from "clsx";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={clsx(inter.className, "antialiased")}>{children}</body>
     </html>
   );
 }
